92a14b9cab83b839ba8031070f058cf5a377e501
Chase Johnston <chasej99@gmail.com>
2020-04-30 20:12:46 -0400

    created django project

---
+++ InvestME/InvestME/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for InvestME project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InvestME.settings')
+
+application = get_asgi_application()
